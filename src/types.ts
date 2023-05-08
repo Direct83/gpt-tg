248d062c513bdb88d91f1a739f711320747025ff
Ivan Vidutin <forumuse@yandex.ru>
2023-05-08 16:17:32 +0500

    del type

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,6 @@
 import { CreateChatCompletionRequest } from 'openai';
 import { TelegrafContext } from 'telegraf-ts';
 
-export interface TGSessionItem {
-	role: string;
-	content?: string;
-}
-
 export interface TGSessionItems {
 	[key: string]: CreateChatCompletionRequest['messages'];
 }
